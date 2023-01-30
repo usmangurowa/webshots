7eb83f3aed407ada307918a3491506cb150b290d
Usman Hassan <usmanhassangu@gmail.com>
2023-01-30 15:48:06 +0100

    perf: implement responsiveness

---
+++ components/Nav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="border-b border-primary/50">
+      <div className="flex items-center justify-between container py-5 ">
+        <span className="text-primary text-2xl font-semibold">WebShots</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "res.cloudinary.com"],
   },
 };
