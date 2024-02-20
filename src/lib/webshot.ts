23727d3ff6b284c500f9eb77ad1fbf4e9385d92e
Usman Hassan <usmanhassangu@gmail.com>
2024-02-20 21:40:19 +0100

    Refactor webshot.ts to use dynamic API URL

--- src/lib/webshot.ts
+++ src/lib/webshot.ts
@@ -2,18 +2,20 @@
 
 import axios from "axios";
 
+const api =
+    process.env.NODE_ENV === "production"
+        ? "https://webshots.vercel.app/"
+        : "http://localhost:3000";
+
 class Webshot {
     constructor(private url: string, private config: ScreenshotsRequestBody) {}
 
     async takeScreenshot() {
         try {
-            const response = await axios.post(
-                "http://localhost:3000/api/webshot",
-                {
-                    ...this.config,
-                    url: this.url,
-                }
-            );
+            const response = await axios.post(`${api}/api/webshot`, {
+                ...this.config,
+                url: this.url,
+            });
 
             return response.data;
         } catch (error) {
